97b4763277e12c17618adde87ab2386c4eea2821
Régis Leandro Rodrigues <r.leandrorodrigues@outlook.com>
2024-05-16 12:38:48 -0300

    primeiro commit

---
+++ MeuSiteMVC_1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MeuSiteMVC_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuSiteMVC_1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Régis Leandro";
+            home.Email = "r.leandrorodrigues@outlook.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MeuSiteMVC_1/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuSiteMVC_1.Models
+{
+    public class HomeModel
+    {
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+    }
+}
